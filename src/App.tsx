338efe31c1e24caa886f98fee3ad42d79ac9c5ca
Arpit Verma <arpitv970@gmail.com>
2023-01-19 15:41:03 +0530

    Created responsive UI for Input Bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import InputBar from './components/InputBar';
 const App: React.FC = () => {
     return (
         <div className='font-poppins bg-primary h-[100vh] w-full overflow-hidden text-white'>
-            <h1 className='sm:text-3xl text-5xl font-bold text-center mx-auto my-[2%]'>
-                TO-DO React App + TS
+            <h1 className='xs:text-[0.5rem] sm:text-[2rem] md:text-[3rem] text-[5rem] font-bold text-center mx-auto sm:mt-[8%]  my-[2%]'>
+                TO-DO <br className='sm:block md:hidden hidden' /> React App + TS
             </h1>
             <InputBar />
+            <div className='w-[75vw] h-0 items-center justify-center mx-auto sm:mt-[2rem] mt-[3rem] border border-dimWhite opacity-25 rounded-full' />
         </div>
     );
 };
--- src/components/InputBar.tsx
+++ src/components/InputBar.tsx
@@ -1,11 +1,11 @@
 const InputBar = () => {
     return (
-        <span className='flex items-center mx-auto p-auto justify-evenly w-[80vw]'>
+        <span className='flex sm:flex-col sm:h-[max] items-center mx-auto p-auto justify-evenly sm:w-[70vw] w-[80vw]'>
             <input
-                className='text-dimWhite bg-dimBlue hover:border-white transition-all duration-300 ease-in-out items-center mx-auto px-[1.5rem] py-1 w-[90%] h-max text-[2rem] rounded-3xl outline-none border-2 border-dimWhite'
+                className='text-dimWhite placeholder-dimWhite placeholder:opacity-25 bg-dimBlue hover:border-white opacity-90 transition-all duration-300 ease-in-out items-center sm:my-5 mx-auto px-[1.5rem] py-1 w-[90%] h-max sm:text-[1.8rem] text-[2rem] rounded-3xl outline-none border-2 border-dimWhite'
                 placeholder='Enter your Task'
             ></input>
-            <button className='text-[1.2rem] border-2 border-dimWhite hover:bg-dimBlue hover:border-white transition-all duration-300 ease-in-out rounded-3xl flex items-center justify-center text-center w-[5rem] h-[3.5rem] my-auto mx-auto'>
+            <button className='text-[1.3rem] font-semibold border-2 border-dimWhite hover:bg-dimBlue opacity-70 hover:opacity-100 transition-all duration-300 ease-in-out rounded-3xl flex items-center justify-center text-center w-[5rem] h-[3.5rem] sm:mt-2 my-auto mx-auto mb-0'>
                 Add
             </button>
         </span>

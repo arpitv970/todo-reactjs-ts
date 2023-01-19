67ef307ed692f3e079fbe86edcbc55d573e661ce
Arpit Verma <arpitv970@gmail.com>
2023-01-19 15:51:28 +0530

    Created responsive UI for Input Bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import InputBar from './components/InputBar';
 
 const App: React.FC = () => {
     return (
-        <div className='font-poppins bg-primary h-[100vh] w-full overflow-hidden text-white'>
-            <h1 className='xs:text-[0.5rem] sm:text-[2rem] md:text-[3rem] text-[5rem] font-bold text-center mx-auto sm:mt-[8%]  my-[2%]'>
+        <div className='font-poppins bg-primary h-[100vh] w-full overflow-hidden text-white selection:bg-violet-800 leading-relaxed'>
+            <h1 className='sm:leading-[3rem] sm:text-[2rem] md:text-[3rem] text-[5rem] font-bold text-center sm:mx-2 md:mx-5 mx-auto sm:mt-[8%]  my-[2%]'>
                 TO-DO <br className='sm:block md:hidden hidden' /> React App + TS
             </h1>
             <InputBar />

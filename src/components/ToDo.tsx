8a6b58748b2cc80306a2adfe340a4ad12375c5b1
Arpit Verma <arpitv970@gmail.com>
2023-01-19 22:05:10 +0530

    Added functionality for deleting the todo task

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -27,6 +27,10 @@ const ToDo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
         );
     };
 
+    const deleteTodo = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
     return (
         <form className='sm:text-[1.3rem] md:text-[1.5rem] text-[1.8rem] border-2 border-white rounded-xl text-white opacity-70 hover:opacity-100 transition-all duration-200 ease-in-out flex justify-around items-center mx-auto sm:w-[90vw] w-[100%] h-max px-[2rem]'>
             {todo.isDone === true ? (
@@ -36,7 +40,10 @@ const ToDo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
             )}
             <span className='h-[100%] my-5 flex justify-between w-[10rem]'>
                 <FiEdit3 className='hover:cursor-pointer' />
-                <MdDelete className='hover:cursor-pointer' />
+                <MdDelete
+                    onClick={() => deleteTodo(todo.id)}
+                    className='hover:cursor-pointer'
+                />
                 <BsCheckCircle
                     onClick={() => doneTodo(todo.id)}
                     className='hover:cursor-pointer'
